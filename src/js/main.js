ff98f5fb2af26e4e4c511b3b17869d5fbb086bfd
fanyong557 <fanyong557@pingan.com.cn>
2015-08-13 19:35:20 +0800

    create view

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,39 @@
 $(function(){
-	
+	var TODO = { };
+
+	(function(TODO){
+
+		var userid = 0;
+
+		var View = Backbone.View.extend({
+			register: function(state) {
+				this.state = state;
+				return this;
+			}
+		});
+
+		var User = Backbone.View.extend({
+			defaults: {
+				username: '默认用户名'
+			},
+
+			initialize: function() {
+				if(!this.get('username')) {
+					this.set({'username': this.defaults.username});
+				}
+				if(!this.get('userid')) {
+					this.set({'userid': ++userid});
+				}
+			}
+		});
+
+		TODO.App = Backbone.Router.extend({
+			
+		});
+
+
+	})(TODO);
+
+	new TODO.App('body');
+	Backbone.history.start();
 });
